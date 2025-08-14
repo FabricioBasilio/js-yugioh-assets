8ab217163a77522a612fc210401388954bd7a137
Fabricio Basilio <fabriciobasilioo@gmail.com>
2025-08-14 14:12:37 -0300

    feat: implementação das funções para colocar as cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,40 +18,81 @@ const state = {
   },
 };
 
-const pathImages = "./src/assets/icons/"
+const pathImages = "./src/assets/icons/";
 
 const cardData = [
-    {
-        id: 0,
-        name: "Blue Eyes White Dragon",
-        type: "Paper",
-        img: `${pathImages}dragon.png`,
-        winOf: [1],
-        loseOf: [2]
-    },
-    {
-        id: 1,
-        name: "Dark Magician",
-        type: "Rock",
-        img: `${pathImages}magician.png`,
-        winOf: [2],
-        loseOf: [0]
-    },
-    {
-        id: 2,
-        name: "Exodia",
-        type: "Scissors",
-        img: `${pathImages}exodia.png`,
-        winOf: [0],
-        loseOf: [1]
-    },
-]
+  {
+    id: 0,
+    name: "Blue Eyes White Dragon",
+    type: "Paper",
+    img: `${pathImages}dragon.png`,
+    winOf: [1],
+    loseOf: [2],
+  },
+  {
+    id: 1,
+    name: "Dark Magician",
+    type: "Rock",
+    img: `${pathImages}magician.png`,
+    winOf: [2],
+    loseOf: [0],
+  },
+  {
+    id: 2,
+    name: "Exodia",
+    type: "Scissors",
+    img: `${pathImages}exodia.png`,
+    winOf: [0],
+    loseOf: [1],
+  },
+];
 
-// const players = {
-//     player1: "player-cards",
+const playerSides = {
+  player1: "player-cards",
+  computer: "computer-cards",
+};
 
-// }
-
-function init() {}
+function init() {
+  drawCards(5, playerSides.player1);
+  drawCards(5, playerSides.computer);
+}
 
 init();
+
+async function drawCards(cardNumbers, fieldSide) {
+  let i;
+  for (i = 0; i < cardNumbers; i++) {
+    const randomIdCard = await getRandomCardId();
+    const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+    document.getElementById(fieldSide).appendChild(cardImage)
+  }
+}
+
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+    return cardData[randomIndex].id;
+}
+
+async function createCardImage(idCard, fieldSide) {
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px")
+    cardImage.setAttribute("src", "./src/assets/icons/card-back.png")
+    cardImage.setAttribute("data-id", idCard)
+    cardImage.classList.add("card")
+
+    if (fieldSide === playerSides.player1) {
+        cardImage.addEventListener("click", () => {
+            setCardsField(cardImage.getAttribute("data-id"))
+        })
+    }
+
+    cardImage.addEventListener("mouseover", () => {
+        drawSelectCard(idCard)
+    })
+
+    
+
+    return cardImage;
+    
+}
