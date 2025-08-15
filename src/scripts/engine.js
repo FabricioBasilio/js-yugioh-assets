e97b482d9f176262014250668a672cab506acf54
Fabricio Basilio <fabriciobasilioo@gmail.com>
2025-08-14 23:30:14 -0300

    feat: Adição do favicon, de música de fundo e estilização do cursor

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -63,6 +63,9 @@ function init() {
 
   drawCards(5, playerSides.player1);
   drawCards(5, playerSides.computer);
+
+  const bgm = document.getElementById("bgm");
+  bgm.play();
 }
 
 async function drawCards(cardNumbers, fieldSide) {
